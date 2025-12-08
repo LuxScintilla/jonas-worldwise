63320abd02f68d2e26ea0ded022691cadc59e4d9
LuxScintilla <catherina.ea@gmail.com>
2025-12-08 14:25:45 +0000

    initial commit

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,10 @@
+import PageNav from "../components/PageNav";
+
+export default function Homepage() {
+  return (
+    <div>
+      <h1>WorldWise Homepage</h1>
+      <PageNav />
+    </div>
+  );
+}
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,10 @@
+import PageNav from "../components/PageNav";
+
+export default function PageNotFound() {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <PageNav />
+    </div>
+  );
+}
